5b45cb77a6a8dda2e8ce82dff605eb24f582a165
J3llyBe4n <zza0423@gmail.com>
2023-07-17 09:42:54 +0900

    데이터 정합성 체크 모듈 테스트 구동

--- routers/check_data_router.py
+++ routers/check_data_router.py
@@ -10,72 +10,74 @@ router = APIRouter()
 # 88몇개 팀갯수 이새끼 다른함수 만들어야함
 @router.get("/check/teams-statistics/")
 async def check_team_statics_data(dir:str, cnt:str, date:str):
+    return 1
 
 #55개 리그 갯수
 @router.get("/check/teams/")
-async def check_team_data(dir:str, cnt:str):
+async def check_team_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/teams", cnt)
 
 #리그 갯수
 @router.get("/check/standings/")
-async def check_standing_data(dir:str, cnt:str):
+async def check_standing_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/standings", cnt)
 
 
 #당일 경기갯수
 @router.get("/check/predictions/")
-async def check_predictions_data(dir:str, date:str):
+async def check_predictions_data(date:str):
     return check_today_Fdata("/api/app/datas/json/season_22/predictions", date)
 
 #리그 갯수
 @router.get("/check/players-topscorers/")
-async def check_topscorers_data(dir:str, cnt:str):
+async def check_topscorers_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/players_topscorers", cnt)
 
    
 # 팀갯수 88몇개
 @router.get("/check/players-squads/")
-async def check_squads_data(dir:str, cnt:str):
+async def check_squads_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/players_squads", cnt)
 
 
 # 5 * 팀 갯수
 @router.get("/check/players/")
 async def check_players_data(dir:str, cnt:str):
+    return 1
 
 # 리그갯수
 @router.get("/check/leagues/")
-async def check_leagues_data(dir:str, cnt:str):
+async def check_leagues_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/leauges", cnt)
 
 
 # 리그 갯수
 @router.get("/check/fixtures/")
-async def check_fixtures_data(dir:str, cnt:str):
+async def check_fixtures_data(cnt:str):
     return check_clean_data("/api/app/datas/json/season_22/fixtures", cnt)
 
 
 # 당일 경기 갯수
 @router.get("/check/fixtures-events/")
-async def check_events_data(dir:str, date:str):
+async def check_events_data(date:str):
     return check_today_Fdata("api/app/datas/json/season_22/fixtures_events", date)
 
 # 당일 경기 갯수
 @router.get("/check/fixtures-headtohead/")
-async def check_headtohead_data(dir:str, date:str):
+async def check_headtohead_data(date:str):
     return check_today_Fdata("api/app/datas/json/season_22/fixture_headtohead", date)
 
 # 당일 경기 갯수
 @router.get("/check/fixtures-lineups/")
-async def check_lineups_data(dir:str, date: str):
+async def check_lineups_data(date: str):
     return check_today_Fdata("api/app/datas/json/season_22/fixture_lineups", date)
 
 # 당일 경기 갯수
 @router.get("/check/fixtures-players/")
-async def check_fplayers_data(dir:str, date: str):
+async def check_fplayers_data(date: str):
     return check_today_Fdata("api/app/datas/json/season_22/fixture_players", date)
 
 # 당일 경기 갯수
 @router.get("/check/fixtures-statistics/")
-async def check_fsattistics_data(dir:str, date:str):
+async def check_fsattistics_data(date:str):
     return check_today_Fdata("api/app/datas/json/season_22/fixtures_statistics", date)
